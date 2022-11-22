8e8bea2ee350d5b50899f00eb993aea1ac9d9efa
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-11-21 22:16:48 -0400

    version correction

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 //GLOBALS
 const GLint WIDTH = 1280, HEIGHT = 720;
-const int VersionMaj = 3, VersionMin = 3;
+const int VersionMaj = 4, VersionMin = 1;
 const char*  glsl_version = "330";
 
 int main(){
