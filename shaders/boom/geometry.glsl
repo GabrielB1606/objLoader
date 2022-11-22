a5ead5f26ef84073b7208750eeadc45771d9e95f
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-11-21 23:42:22 -0400

    clean up boom shader

--- shaders/boom/geometry.glsl
+++ shaders/boom/geometry.glsl
@@ -3,18 +3,6 @@
 layout (triangles) in;
 layout (triangle_strip, max_vertices = 3) out;
 
-out vec3 vs_position;
-out vec3 vs_color;
-out vec2 vs_textcoord;
-out vec3 vs_normal;
-
-in DATA{
-    vec3 position;
-    vec3 normal;
-    vec3 color;
-    vec2 textcoord;
-} data_in[];
-
 uniform mat4 ModelMatrix;
 uniform mat4 ViewMatrix;
 uniform mat4 ProjectionMatrix;
@@ -34,10 +22,8 @@ void main(){
         gl_Position =
             ProjectionMatrix * ViewMatrix * ModelMatrix *
             (gl_in[i].gl_Position+ explosionScale*vec4(face_normal, 0.0f));
-        
-        vs_color = data_in[i].color;
+
         EmitVertex();
-        
     }
     
     EndPrimitive();
--- shaders/boom/vertex.glsl
+++ shaders/boom/vertex.glsl
@@ -5,25 +5,8 @@ layout (location = 1) in vec3 vertex_color;
 layout (location = 2) in vec2 vertex_textcoord;
 layout (location = 3) in vec3 vertex_normal;
 
-// out vec3 vs_position;
-// out vec3 vs_color;
-// out vec2 vs_textcoord;
-// out vec3 vs_normal;
-
-out DATA{
-    vec3 position;
-    vec3 normal;
-    vec3 color;
-    vec2 textcoord;
-} data_out;
-
 void main(){
-    data_out.position = vertex_position;
-    data_out.normal = vertex_normal;
-    data_out.color = vertex_color;
-    data_out.textcoord = vertex_textcoord;
+
     gl_Position = vec4(vertex_position, 1.f);
 
-    // // vs_normal = mat3(ModelMatrix) * vertex_normal;
-    // // vs_normal = mat3(transpose(inverse(ModelMatrix))) * vertex_normal;
 }
